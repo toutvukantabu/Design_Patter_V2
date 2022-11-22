40d353c5d0cb1a91a4eeddc6ff4c7493fe4cc2cf
Gwendal Bescont <64533628+toutvukantabu@users.noreply.github.com>
2022-11-22 15:41:51 +0100

    Update jsonFormater.php

--- src/StrategyPattern/Strategy/jsonFormater.php
+++ src/StrategyPattern/Strategy/jsonFormater.php
@@ -5,17 +5,16 @@ namespace App\StrategyPattern\Strategy;
 
 use App\StrategyPattern\Interface\StrategyInterface;
 
-
-class jsonFormater  implements StrategyInterface
+class JsonFormater  implements StrategyInterface
 {
 
     public function Transform(array $data): string
     {
-
         return  json_encode($data);
     }
+
     public function support(string $param): bool
     {
-        return $param = 'json';
+        return 'Json' === $param;
     }
 }
