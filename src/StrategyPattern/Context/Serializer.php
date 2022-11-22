d5a09c424fbca4ab55677fb706b67b00dd8abf60
Gwendal Bescont <64533628+toutvukantabu@users.noreply.github.com>
2022-11-22 15:39:12 +0100

    Update Serializer.php

--- src/StrategyPattern/Context/Serializer.php
+++ src/StrategyPattern/Context/Serializer.php
@@ -17,53 +17,65 @@ class Serializer
     private ?array $definedFormater = [];
     private ?array $definedNormalizer = [];
     private ?array $datasFormated = [];
-    private mixed $dataFormated;
+    private $dataFormated = null;
+    private ?array $dataNormalized = null;
 
-    private array $formater = [jsonFormater::class, XmlFormater::class, PlainTextFormater::class];
+    private array $formater = [JsonFormater::class, PlainTextFormater::class, ArrayFormater::class, LogsFormater::class];
 
-    private array $normalizer = [JsonNormalizer::class, ObjectNormalizer::class, XmlNormalizer::class];
+    private array $normalizer = [JsonNormalizer::class, ObjectNormalizer::class, StringNormalizer::class];
 
+    /**
+     * Undocumented function
+     *
+     * @param array|null $definedFormater
+     * @param array|null $definedNormalizer
+     */
     public  function __construct(array $definedFormater = null,  array $definedNormalizer = null)
     {
 
         if ($definedFormater) $this->definedNormalizer = $definedNormalizer;
         if ($definedNormalizer) $this->definedFormater = $definedFormater;
     }
-
-    public function serialize(mixed $object, string $format): self
+    /**
+     * Undocumented function
+     *
+     * @param null||string||object||array $object
+     * @param string $support 'PlainText || Array || Json
+     * @return null||string||object||array
+     * 
+     */
+    public function serialize($object, string $support)
     {
-        if (!$this->definedFormater && !$format) {
-
-            echo 'Erreur format required';
+        if (empty($object) || null === $object || !$object) {
+            return null;
         }
-
+        if ($object instanceof BuilderInterface) {
+            $object = $object->getBuild();
+        }
+        
+        if (!$this->definedFormater && !$support) {
+            throw new NotFoundException('Formater');
+        }
         $normalizer  = $this->normalizer ?? $this->definedNormalizer;
         $formater = $this->formater ?? $this->definedFormater;
-
-        $countFormater = count($formater);
-
         foreach ($normalizer  as $normalize) {
             foreach ($formater as $format) {
-                $normalizerClass  = new $normalize;
-                $formaterClass = new $format;
-                // dump($normalizer instanceof NormalizerInterface ,$formaterClass instanceof StrategyInterface);
-
-                $datanormalized = $normalizerClass->normalize($object);
-                if (is_array($datanormalized) ) {
-
-                    $this->dataFormated = $formaterClass->transform($datanormalized);
-dump($this->dataFormated);
-                    if ($countFormater > 1) {
-
-                        array_push($this->datasFormated, $this->dataFormated);
+                
+                if (!\is_array($object) || \is_object($object) && $this->dataNormalized = null) {
+                    $normalizerClass  = new $normalize;
+                    $this->dataNormalized = $normalizerClass->normalize($object);
+                }
+                
+                if (\is_array($this->dataNormalized)  && null != $this->dataNormalized) {
+                    $formaterClass = new $format;
+                    if ($formaterClass->support($support)) {
+                        $this->dataFormated = $formaterClass->transform($this->dataNormalized);
                     }
                 }
             }
         }
-
-        return $this;
+        return $this->dataFormated;
     }
-
 
     /**
      * Get the value of definedFormater
@@ -85,7 +97,7 @@ dump($this->dataFormated);
 
     /**
      * Get the value of datasFormated
-     */ 
+     */
     public function getDatasFormated()
     {
         return $this->datasFormated;
@@ -93,9 +105,11 @@ dump($this->dataFormated);
 
     /**
      * Get the value of dataFormated
-     */ 
+     */
     public function getDataFormated()
     {
         return $this->dataFormated;
     }
+
 }
+
